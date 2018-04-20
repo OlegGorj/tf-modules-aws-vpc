b6acf89c53a0450c99b72d9bf754f5191a09e39b
Oleg Gorodnitchi <oleg.gorodnitchi@sk.ibm.com>
2018-04-21 01:23:44 +0200

    fixes and tests

--- main.tf
+++ main.tf
@@ -16,7 +16,9 @@ resource "aws_vpc" "default" {
 resource "aws_internet_gateway" "default" {
 
   vpc_id = "${aws_vpc.default.id}"
-  tags   = "IGW-VPC-${aws_vpc.default.id}"
+  tags = {
+    Name   = "IGW-VPC-${aws_vpc.default.id}"
+  }
 
 }
 
